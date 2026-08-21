2c5ff510cf1ba8a33f869b88d8d2e83d882e8bb6
rakesh <rk92057sh@gmail.com>
2026-08-21 14:19:20 +0530

    dashboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -183,9 +183,10 @@ export default function Dashboard(){
     }
     }
     return (
-        <div className="min-h-screen bg-gray-100 dark:bg-gray-950 p-10 transition-colors">
-            <div className="min-h-screen bg-gray-100 dark:bg-gray-950 transition-colors">
-                <DashboardNavbar/>
+        <div className="min-h-screen bg-gray-100 dark:bg-gray-950 transition-colors">
+            <DashboardNavbar/>
+
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
             
                 <SearchRepository
                     owner ={owner}
